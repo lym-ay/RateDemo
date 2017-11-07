c64eeda17a955a53527126cf25170ebe1c9885e7
yanminli <>
2017-11-07 11:06:26 +0800

    add new program
    this program show rate.input the number through voice.

---
+++ RateDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RateDemo
+//
+//  Created by olami on 2017/11/7.
+//  Copyright © 2017年 VIA Technologies, Inc. & OLAMI Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
